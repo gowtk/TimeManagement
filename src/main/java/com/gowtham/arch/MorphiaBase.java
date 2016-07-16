396ab008805bbfc7a6d8db8b014d031748f6bd4f
Gowtham Kittusamy <kgowthamid@gmail.com>
2016-07-16 10:27:02 +0530

    app config added

---
+++ src/main/java/com/gowtham/arch/MorphiaBase.java
@@ -0,0 +1,21 @@
+package com.gowtham.arch;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.mongodb.MongoClient;
+
+public class MorphiaBase {
+
+	Morphia morphia;
+
+	public MorphiaBase() {
+		morphia = new Morphia();
+		morphia.mapPackage("com.gowtham.bean");
+
+		// create the Datastore connecting to the default port on the local host
+	/*	final Datastore datastore = morphia.createDatastore(new MongoClient(), "morphia_example");
+		datastore.ensureIndexes();*/
+	}
+
+}
