b2e181305f9f0b388096420711b9c384bf9bf7fe
Gowtham Kittusamy <kgowthamid@gmail.com>
2016-07-16 10:27:02 +0530

    app config added

---
+++ src/main/java/com/gowtham/spring/AppInitializer.java
@@ -0,0 +1,33 @@
+package com.gowtham.spring;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		Filter[] singleton = { new AppFilter() };
+		return singleton;
+	}
+
+}
